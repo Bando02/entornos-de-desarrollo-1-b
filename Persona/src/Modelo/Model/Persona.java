374ca35327eadf31da20251f17197252742d6d44
fvp02 <152099617+Bando02@users.noreply.github.com>
2024-03-14 14:13:55 +0100

    Add files via upload

---
+++ Persona/src/Modelo/Model/Persona.java
@@ -0,0 +1,58 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private String dni;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String apellido1;
+
+    /**
+     * 
+     */
+    private String apellido2;
+
+    /**
+     * 
+     */
+    private int edad;
+
+    /**
+     * @param String dni 
+     * @param String nombre 
+     * @param String apellido1 
+     * @param String apellido2 
+     * @param int edad
+     */
+    public void Persona(void String dni, void String nombre, void String apellido1, void String apellido2, void int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @param dni
+     */
+    public void dni(string dni) {
+        // TODO implement here
+    }
+
+}
