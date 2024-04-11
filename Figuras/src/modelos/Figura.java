2e9c9a9d908ba7dc042031de0c52832bac808724
fvp02 <152099617+Bando02@users.noreply.github.com>
2024-04-11 10:59:46 +0200

    Figuras
    Java Proyect de figuras, calcula area y perimetro

---
+++ Figuras/src/modelos/Figura.java
@@ -0,0 +1,26 @@
+package modelos;
+
+/**
+ * Clase que representa una figura geometrica
+ * 
+ * @version 1.0
+ * 
+ * @author Fernando
+ */
+public abstract class Figura {
+	
+	/**
+	 * Devuelve el area de la figura
+	 * 
+	 * @return		el area de la figura, en metros cuadrados
+	 */
+	public abstract double area();
+	
+	/**
+	 * Devuelve el perimetro de la figura
+	 * 
+	 * @return		el perimetro de la figura, en metros cuadrados
+	 */
+	public abstract double perimetro();
+
+}
